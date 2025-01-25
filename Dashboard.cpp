6432ebeefb15a89f11bcc48a0cc65f40abd3cfa4
hyacinthechem <chemaslehyacinthe@gmail.com>
2025-01-25 22:59:04 +1300

    Action Listener for Expense Upload Button

--- Dashboard.cpp
+++ Dashboard.cpp
@@ -15,6 +15,14 @@
 #include <iostream>
 
 //QT Widget Imports
+#include <QWidget> //mainClass
+#include <QPushButton> //for the physical button
+#include <QVBoxLayout> // for layout management ( like with JPane )
+#include <QDialog> //For Dialog windows ( JPane.OptionDialog )
+#include <QLineEdit> //textfields
+#include <QMessageBox>
+#include <QObject>
+
 
 
 using namespace std;
@@ -26,7 +34,7 @@ Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
     button = new QPushButton("Create new Expense");
     button->setDefault(true);
 
-    QObject::connect(button, &QPushButton::clicked,this, Dashboard::onAddExpenseButtonClicked); //the instantiated event listener
+    QObject::connect(button, &QPushButton::clicked,this, &Dashboard::onAddExpenseButtonClicked); //the instantiated event listener
     //passes parameters like the button and QPUshButton method being signalled as clicked. defined within this class. and using the onAddExepenseButton as the slot method
 
     QVBoxLayout *layout = new QVBoxLayout();
@@ -36,6 +44,69 @@ Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
 }
 
 void Dashboard::onAddExpenseButtonClicked() {
+    //create dialog indicating success of button clicked
+    QDialog dialog(this);
+    dialog.setWindowTitle("Add Expense");
+
+    //create the box layout for all the textfields
+    QVBoxLayout *layout = new QVBoxLayout(&dialog);
+
+    //create TextField objects for parameters relating to expense being QLineEdit ( inputs for each )
+
+
+
+    QLineEdit *titleInput = new QLineEdit(&dialog);
+    titleInput->setPlaceholderText("Title Of Expense: ");
+    layout->addWidget(titleInput);
+
+
+    QLineEdit *descriptionInput = new QLineEdit(&dialog);
+    descriptionInput->setPlaceholderText("Description Of Expense: ");
+    layout->addWidget(descriptionInput);
+
+    QLineEdit *datePurchasedInput = new QLineEdit(&dialog);
+    datePurchasedInput ->setPlaceholderText("Date Of Expense: ");
+    layout->addWidget(datePurchasedInput);
+
+    QLineEdit *amountSpentInput = new QLineEdit(&dialog);
+    amountSpentInput->setPlaceholderText("Amount Spent: ");
+    layout->addWidget(amountSpentInput);
+
+    //Operations to save the expense
+
+    QPushButton *saveExpenseButton = new QPushButton("Save Expense");
+    layout->addWidget(saveExpenseButton);
+    dialog.setWindowTitle("Save Expense");
+
+    connect(saveExpenseButton,&QPushButton::clicked,[&]() {
+        //connect is the actionListener in cpp
+QString titleOutput = titleInput->text();
+QString descriptionOutput = descriptionInput->text();
+QString datePurchasedOutput = datePurchasedInput->text();
+QString amountSpentOutput = amountSpentInput->text();
+
+bool validNum;
+double amount = amountSpentOutput.toDouble(&validNum);
+
+dialog.setWindowTitle("Save Expense");
+if(!validNum) {
+    QMessageBox::warning(this,"Non-Numerical", "Enter a valid Number");
+}else if(amount < 0) {
+    QMessageBox::warning(this, "Negative Amount", "Amout must be Positive");
+}
+if(titleOutput.isEmpty() || descriptionOutput.isEmpty() || datePurchasedOutput.isEmpty() || amountSpentOutput.isEmpty()) {
+ QMessageBox::warning(this,"Input Error","Please Enter a valid Expense");
+}
+        dialog.exec();
+    });
+
+
+
+
+
+
+
+
 
 }
 
